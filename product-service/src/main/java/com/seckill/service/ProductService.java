38094818346b67d7ccf15fb3815610961bca0651
ayako17 <vitae-aqua@outlook.com>
2026-03-14 17:02:41 +0800

    refactor: 重组项目结构，将后端代码移至 seckill-backend 目录

--- product-service/src/main/java/com/seckill/service/ProductService.java
+++
@@ -1,7 +0,0 @@
-package com.seckill.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.seckill.entity.Product;
-
-public interface ProductService extends IService<Product> {
-}
